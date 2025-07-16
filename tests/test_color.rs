5efe1f5bb335ef0e7dc5c79371debec119a0099c
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 06:55:57 +0000

    fix tests/test_color.rs line 124, such that "0);" becomes "202);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -121,7 +121,7 @@ fn test_invert_ansi() {
     assert_eq!(invert_ansi(from_byte(116).into()), 70);
     assert_eq!(invert_ansi(from_byte(117).into()), 65);
     assert_eq!(invert_ansi(from_byte(118).into()), 90);
-    assert_eq!(invert_ansi(from_byte(119).into()), 0);
+    assert_eq!(invert_ansi(from_byte(119).into()), 202);
     // assert_eq!(invert_ansi(from_byte(120).into()), 0);
     // assert_eq!(invert_ansi(from_byte(121).into()), 0);
     // assert_eq!(invert_ansi(from_byte(122).into()), 0);
