4f0ad1eadea293021f2daef68d359fa2d6df26e2
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 02:55:00 +0000

    fix tests/test_color.rs line 226, such that "0" becomes "221);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -223,7 +223,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(218), 218);
     assert_eq!(from_byte(219), 219);
     assert_eq!(from_byte(220), 220);
-    // assert_eq!(from_byte(221), 0);
+    assert_eq!(from_byte(221), 221);
     // assert_eq!(from_byte(222), 0);
     // assert_eq!(from_byte(223), 0);
     // assert_eq!(from_byte(224), 0);
