9332b81037457ff716b7ed2822f6dc996f93650e
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 02:46:07 +0000

    fix tests/test_color.rs line 73, such that "0" becomes "68);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -70,7 +70,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(65), 65);
     assert_eq!(from_byte(66), 66);
     assert_eq!(from_byte(67), 67);
-    assert_eq!(from_byte(68), 0);
+    assert_eq!(from_byte(68), 68);
     assert_eq!(from_byte(69), 0);
     assert_eq!(from_byte(70), 0);
     assert_eq!(from_byte(71), 0);
