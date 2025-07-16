a0f53340a7c2fd44a40548f392b7a057b67bf094
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:13:46 +0000

    fix tests/test_color.rs line 266, such that "[253, 253, 253]" becomes "[218, 218, 218]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -263,7 +263,7 @@ fn test_grayscale() {
     assert_eq!(get_ansi_rgb(250), [188, 188, 188]);
     assert_eq!(get_ansi_rgb(251), [198, 198, 198]);
     assert_eq!(get_ansi_rgb(252), [208, 208, 208]);
-    assert_eq!(get_ansi_rgb(253), [253, 253, 253]);
+    assert_eq!(get_ansi_rgb(253), [218, 218, 218]);
     assert_eq!(get_ansi_rgb(254), [254, 254, 254]);
     assert_eq!(get_ansi_rgb(255), [255, 255, 255]);
 }
