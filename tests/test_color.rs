2abc4ddd7c214848b083f3fd0bb87a5356dbbdd0
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:09:11 +0000

    fix tests/test_color.rs line 190, such that "[181, 181, 181]" becomes "[215, 175, 175]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -187,7 +187,7 @@ fn test_cube() {
     assert_eq!(get_ansi_rgb(178), [215, 175, 0]);
     assert_eq!(get_ansi_rgb(179), [215, 175, 95]);
     assert_eq!(get_ansi_rgb(180), [215, 175, 135]);
-    assert_eq!(get_ansi_rgb(181), [181, 181, 181]);
+    assert_eq!(get_ansi_rgb(181), [215, 175, 175]);
     assert_eq!(get_ansi_rgb(182), [182, 182, 182]);
     assert_eq!(get_ansi_rgb(183), [183, 183, 183]);
     assert_eq!(get_ansi_rgb(184), [184, 184, 184]);
