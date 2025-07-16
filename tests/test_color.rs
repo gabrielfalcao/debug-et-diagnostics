112cf203934c4110221e4c424592b89dc33e8fa8
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:09:03 +0000

    fix tests/test_color.rs line 168, such that "[159, 159, 159]" becomes "[175, 0, 0]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -165,7 +165,7 @@ fn test_cube() {
     assert_eq!(get_ansi_rgb(156), [175, 0, 135]);
     assert_eq!(get_ansi_rgb(157), [175, 0, 175]);
     assert_eq!(get_ansi_rgb(158), [175, 0, 215]);
-    assert_eq!(get_ansi_rgb(159), [159, 159, 159]);
+    assert_eq!(get_ansi_rgb(159), [175, 0, 0]);
     assert_eq!(get_ansi_rgb(160), [160, 160, 160]);
     assert_eq!(get_ansi_rgb(161), [161, 161, 161]);
     assert_eq!(get_ansi_rgb(162), [162, 162, 162]);
