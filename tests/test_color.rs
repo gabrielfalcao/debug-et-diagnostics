0f027e172c4eb7c24ce5939979042c0a3ec52a2d
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 06:34:50 +0000

    uncomment tests/test_color.rs line 7 so as to trigger next fix

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -4,7 +4,7 @@ use debug_et_diagnostics::{invert_ansi, from_byte};
 fn test_invert_ansi() {
     assert_eq!(invert_ansi(from_byte(0).into()), 231);
     assert_eq!(invert_ansi(from_byte(1).into()), 35);
-    // assert_eq!(invert_ansi(from_byte(2).into()), 0);
+    assert_eq!(invert_ansi(from_byte(2).into()), 0);
     // assert_eq!(invert_ansi(from_byte(3).into()), 0);
     // assert_eq!(invert_ansi(from_byte(4).into()), 0);
     // assert_eq!(invert_ansi(from_byte(5).into()), 0);
