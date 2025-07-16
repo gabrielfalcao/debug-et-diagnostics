66adbbc009c7ff60477031fcb9bb6edeb500b390
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 06:59:38 +0000

    uncomment tests/test_color.rs line 176 so as to trigger next fix

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -173,7 +173,7 @@ fn test_invert_ansi() {
     assert_eq!(invert_ansi(from_byte(168).into()), 16);
     assert_eq!(invert_ansi(from_byte(169).into()), 202);
     assert_eq!(invert_ansi(from_byte(170).into()), 109);
-    // assert_eq!(invert_ansi(from_byte(171).into()), 0);
+    assert_eq!(invert_ansi(from_byte(171).into()), 0);
     // assert_eq!(invert_ansi(from_byte(172).into()), 0);
     // assert_eq!(invert_ansi(from_byte(173).into()), 0);
     // assert_eq!(invert_ansi(from_byte(174).into()), 0);
