29a33e0e76d7f85e138f1bedb18cb96537e2a35f
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 08:12:35 +0000

    fix tests/test_color.rs line 238, such that "228);" becomes "18);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -235,7 +235,7 @@ fn test_from_display_number_0_to_255() {
     assert_eq!(from_display(230), 215);
     assert_eq!(from_display(231), 255);
     assert_eq!(from_display(232), 8);
-    assert_eq!(from_display(233), 228);
+    assert_eq!(from_display(233), 18);
     // assert_eq!(from_display(234), 213);
     // assert_eq!(from_display(235), 203);
     // assert_eq!(from_display(236), 198);
