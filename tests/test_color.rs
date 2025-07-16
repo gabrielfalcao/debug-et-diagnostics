84c22d8da4beac18c223cf3417f6a26578c81239
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:08:23 +0000

    fix tests/test_color.rs line 51, such that "[42, 42, 42]" becomes "[0, 215, 135]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -48,7 +48,7 @@ fn test_cube() {
     assert_eq!(get_ansi_rgb(39), [0, 175, 0]);
     assert_eq!(get_ansi_rgb(40), [0, 215, 0]);
     assert_eq!(get_ansi_rgb(41), [0, 215, 95]);
-    assert_eq!(get_ansi_rgb(42), [42, 42, 42]);
+    assert_eq!(get_ansi_rgb(42), [0, 215, 135]);
     assert_eq!(get_ansi_rgb(43), [43, 43, 43]);
     assert_eq!(get_ansi_rgb(44), [44, 44, 44]);
     assert_eq!(get_ansi_rgb(45), [45, 45, 45]);
