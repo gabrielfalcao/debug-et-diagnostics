4e67b18fe4e054533f310b885bd60e4c2e029560
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:08:30 +0000

    fix tests/test_color.rs line 72, such that "[63, 63, 63]" becomes "[95, 95, 0]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -69,7 +69,7 @@ fn test_cube() {
     assert_eq!(get_ansi_rgb(60), [95, 95, 135]);
     assert_eq!(get_ansi_rgb(61), [95, 95, 175]);
     assert_eq!(get_ansi_rgb(62), [95, 95, 215]);
-    assert_eq!(get_ansi_rgb(63), [63, 63, 63]);
+    assert_eq!(get_ansi_rgb(63), [95, 95, 0]);
     assert_eq!(get_ansi_rgb(64), [64, 64, 64]);
     assert_eq!(get_ansi_rgb(65), [65, 65, 65]);
     assert_eq!(get_ansi_rgb(66), [66, 66, 66]);
