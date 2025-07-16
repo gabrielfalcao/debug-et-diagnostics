dad5e1c2719b5a9d84a58d2ad0cace2f79ce043f
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 08:06:38 +0000

    fix tests/test_color.rs line 157, such that "65);" becomes "175);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -154,7 +154,7 @@ fn test_from_display_number_0_to_255() {
     assert_eq!(from_display(149), 39);
     assert_eq!(from_display(150), 255);
     assert_eq!(from_display(151), 215);
-    assert_eq!(from_display(152), 65);
+    assert_eq!(from_display(152), 175);
     // assert_eq!(from_display(153), 70);
     // assert_eq!(from_display(154), 131);
     // assert_eq!(from_display(155), 16);
