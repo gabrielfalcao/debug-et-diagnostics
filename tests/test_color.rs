4fa3e54854df274dba928c492f3220dd80dce25d
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 05:01:12 +0000

    fix tests/test_color.rs line 249, such that "244);" becomes "128);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -246,7 +246,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(241), 98);
     assert_eq!(from_byte(242), 108);
     assert_eq!(from_byte(243), 118);
-    assert_eq!(from_byte(244), 244);
+    assert_eq!(from_byte(244), 128);
     // assert_eq!(from_byte(245), 245);
     // assert_eq!(from_byte(246), 246);
     // assert_eq!(from_byte(247), 247);
