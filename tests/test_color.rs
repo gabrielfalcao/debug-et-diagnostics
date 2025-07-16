5abb5fc898cb94da32dd1c04a417b97019bccaaf
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 04:42:12 +0000

    fix tests/test_color.rs line 28, such that "23);" becomes "0);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -25,7 +25,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(20), 215);
     assert_eq!(from_byte(21), 255);
     assert_eq!(from_byte(22), 95);
-    assert_eq!(from_byte(23), 23);
+    assert_eq!(from_byte(23), 0);
     // assert_eq!(from_byte(24), 24);
     // assert_eq!(from_byte(25), 25);
     // assert_eq!(from_byte(26), 26);
