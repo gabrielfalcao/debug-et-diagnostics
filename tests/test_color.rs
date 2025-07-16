3f9ddd494a001240e9f6b15e4c910351383bbb7e
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:09:19 +0000

    fix tests/test_color.rs line 213, such that "[204, 204, 204]" becomes "[0, 95, 135]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -210,7 +210,7 @@ fn test_cube() {
     assert_eq!(get_ansi_rgb(201), [0, 0, 0]);
     assert_eq!(get_ansi_rgb(202), [0, 95, 0]);
     assert_eq!(get_ansi_rgb(203), [0, 95, 95]);
-    assert_eq!(get_ansi_rgb(204), [204, 204, 204]);
+    assert_eq!(get_ansi_rgb(204), [0, 95, 135]);
     assert_eq!(get_ansi_rgb(205), [205, 205, 205]);
     assert_eq!(get_ansi_rgb(206), [206, 206, 206]);
     assert_eq!(get_ansi_rgb(207), [207, 207, 207]);
