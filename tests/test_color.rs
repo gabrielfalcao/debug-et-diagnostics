451280d338627a8e62d460dbb7054197dfbdf1e4
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 02:54:34 +0000

    fix tests/test_color.rs line 185, such that "0" becomes "180);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -182,7 +182,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(177), 177);
     assert_eq!(from_byte(178), 178);
     assert_eq!(from_byte(179), 179);
-    // assert_eq!(from_byte(180), 0);
+    assert_eq!(from_byte(180), 180);
     // assert_eq!(from_byte(181), 0);
     // assert_eq!(from_byte(182), 0);
     // assert_eq!(from_byte(183), 0);
