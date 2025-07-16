cb662a6ea7ad7e7a35fa335c4207941c1edcd375
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:08:40 +0000

    fix tests/test_color.rs line 100, such that "[91, 91, 91]" becomes "[135, 0, 175]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -97,7 +97,7 @@ fn test_cube() {
     assert_eq!(get_ansi_rgb(88), [135, 0, 0]);
     assert_eq!(get_ansi_rgb(89), [135, 0, 95]);
     assert_eq!(get_ansi_rgb(90), [135, 0, 135]);
-    assert_eq!(get_ansi_rgb(91), [91, 91, 91]);
+    assert_eq!(get_ansi_rgb(91), [135, 0, 175]);
     assert_eq!(get_ansi_rgb(92), [92, 92, 92]);
     assert_eq!(get_ansi_rgb(93), [93, 93, 93]);
     assert_eq!(get_ansi_rgb(94), [94, 94, 94]);
