ba566d27f88e2e77c058f19f7c1cca299b87f7a4
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 04:57:09 +0000

    fix tests/test_color.rs line 203, such that "198);" becomes "120);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -200,7 +200,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(195), 215);
     assert_eq!(from_byte(196), 255);
     assert_eq!(from_byte(197), 160);
-    assert_eq!(from_byte(198), 198);
+    assert_eq!(from_byte(198), 120);
     // assert_eq!(from_byte(199), 199);
     // assert_eq!(from_byte(200), 200);
     // assert_eq!(from_byte(201), 201);
