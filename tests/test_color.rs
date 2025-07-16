67e40d708b8c1819ea644b7da980373c74c231c3
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 08:10:09 +0000

    fix tests/test_color.rs line 205, such that "207);" becomes "40);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -202,7 +202,7 @@ fn test_from_display_number_0_to_255() {
     assert_eq!(from_display(197), 160);
     assert_eq!(from_display(198), 120);
     assert_eq!(from_display(199), 80);
-    assert_eq!(from_display(200), 207);
+    assert_eq!(from_display(200), 40);
     // assert_eq!(from_display(201), 231);
     // assert_eq!(from_display(202), 87);
     // assert_eq!(from_display(203), 231);
