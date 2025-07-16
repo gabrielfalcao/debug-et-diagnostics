3abf3262b085f98b76225323d8c1d5f30d689736
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 00:13:45 +0000

    fix tests/test_color.rs line 260, such that "[247, 247, 247]" becomes "[158, 158, 158]"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -257,7 +257,7 @@ fn test_grayscale() {
     assert_eq!(get_ansi_rgb(244), [128, 128, 128]);
     assert_eq!(get_ansi_rgb(245), [138, 138, 138]);
     assert_eq!(get_ansi_rgb(246), [148, 148, 148]);
-    assert_eq!(get_ansi_rgb(247), [247, 247, 247]);
+    assert_eq!(get_ansi_rgb(247), [158, 158, 158]);
     assert_eq!(get_ansi_rgb(248), [248, 248, 248]);
     assert_eq!(get_ansi_rgb(249), [249, 249, 249]);
     assert_eq!(get_ansi_rgb(250), [250, 250, 250]);
