f930532d5b086434ca4b9555880a8f5fcc2c0406
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 04:47:31 +0000

    fix tests/test_color.rs line 91, such that "86);" becomes "119);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -88,7 +88,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(83), 255);
     assert_eq!(from_byte(84), 39);
     assert_eq!(from_byte(85), 15);
-    assert_eq!(from_byte(86), 86);
+    assert_eq!(from_byte(86), 119);
     // assert_eq!(from_byte(87), 87);
     // assert_eq!(from_byte(88), 88);
     // assert_eq!(from_byte(89), 89);
