299a59c38f6d2984cecf2c493392c0107cdfbb1b
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-16 08:11:08 +0000

    uncomment tests/test_color.rs line 219 so as to trigger next fix

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -216,7 +216,7 @@ fn test_from_display_number_0_to_255() {
     assert_eq!(from_display(211), 215);
     assert_eq!(from_display(212), 175);
     assert_eq!(from_display(213), 135);
-    // assert_eq!(from_display(214), 131);
+    assert_eq!(from_display(214), 131);
     // assert_eq!(from_display(215), 16);
     // assert_eq!(from_display(216), 25);
     // assert_eq!(from_display(217), 231);
