828cd7718ddc7cb062d14a43ceb55206ee34b1a8
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:57:06 +0000

    fix tests/test_color.rs line 9, such that "0);" becomes "39);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -6,7 +6,7 @@ fn test_from_debug_letter_A_to_Z() {
     assert_eq!(from_debug("B"), 95);
     assert_eq!(from_debug("C"), 119);
     assert_eq!(from_debug("D"), 15);
-    assert_eq!(from_debug("E"), 0);
+    assert_eq!(from_debug("E"), 39);
     // assert_eq!(from_debug("F"), 255);
     // assert_eq!(from_debug("G"), 175);
     // assert_eq!(from_debug("H"), 119);
