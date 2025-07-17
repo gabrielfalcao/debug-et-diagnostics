477976866ea1144d8a1d96bf78b5869d9522e84d
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 11:49:58 +0000

    fix tests/test_color.rs line 26, such that "0);" becomes "119);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -23,7 +23,7 @@ fn test_from_display_letter_A_to_Z() {
     assert_eq!(from_display("S"), 255);
     assert_eq!(from_display("T"), 39);
     assert_eq!(from_display("U"), 15);
-    assert_eq!(from_display("V"), 0);
+    assert_eq!(from_display("V"), 119);
     // assert_eq!(from_display("W"), 0);
     // assert_eq!(from_display("X"), 0);
     // assert_eq!(from_display("Y"), 0);
