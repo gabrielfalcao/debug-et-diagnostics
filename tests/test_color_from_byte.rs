4016f2f5e3c56deb901452fb46f183b5c2029fa4
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:21:25 +0000

    fix tests/test_color_from_byte.rs line 11, such that "0);" becomes "6);"

--- tests/test_color_from_byte.rs
+++ tests/test_color_from_byte.rs
@@ -8,7 +8,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(3), 3);
     assert_eq!(from_byte(4), 4);
     assert_eq!(from_byte(5), 5);
-    assert_eq!(from_byte(6), 0);
+    assert_eq!(from_byte(6), 6);
     // assert_eq!(from_byte(7), 229);
     // assert_eq!(from_byte(8), 127);
     // assert_eq!(from_byte(9), 255);
