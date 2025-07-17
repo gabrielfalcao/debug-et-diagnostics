47a86d75d30f54aa5b46c3eccaedee0384afb8a8
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 17:37:19 +0000

    fix tests/test_color_get_ansi_rgb.rs line 225, such that "[0, 175, 135]);" becomes "[255, 175, 135]);"

--- tests/test_color_get_ansi_rgb.rs
+++ tests/test_color_get_ansi_rgb.rs
@@ -222,7 +222,7 @@ fn test_cube() {
     assert_get_ansi_rgb!(213, [255, 135, 255]);
     assert_get_ansi_rgb!(214, [255, 175, 0]);
     assert_get_ansi_rgb!(215, [255, 175, 95]);
-    assert_get_ansi_rgb!(216, [0, 175, 135]);
+    assert_get_ansi_rgb!(216, [255, 175, 135]);
     assert_get_ansi_rgb!(217, [0, 175, 175]);
     assert_get_ansi_rgb!(218, [0, 175, 215]);
     assert_get_ansi_rgb!(219, [0, 175, 0]);
