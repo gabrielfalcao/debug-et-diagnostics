34a99b9f5642e7003cfbac918955f717ea958caf
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:23:37 +0000

    fix tests/test_color_from_byte.rs line 123, such that "120);" becomes "118);"

--- tests/test_color_from_byte.rs
+++ tests/test_color_from_byte.rs
@@ -120,7 +120,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(115), 115);
     assert_eq!(from_byte(116), 116);
     assert_eq!(from_byte(117), 117);
-    assert_eq!(from_byte(118), 120);
+    assert_eq!(from_byte(118), 118);
     // assert_eq!(from_byte(119), 39);
     // assert_eq!(from_byte(120), 255);
     // assert_eq!(from_byte(121), 215);
