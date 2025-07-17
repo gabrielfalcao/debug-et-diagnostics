37ddd4286dd85b3777f7d3e4586b78cea7acdbe4
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 11:49:46 +0000

    fix tests/test_color.rs line 13, such that "0);" becomes "95);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -10,7 +10,7 @@ fn test_from_display_letter_A_to_Z() {
     assert_eq!(from_display("F"), 255);
     assert_eq!(from_display("G"), 175);
     assert_eq!(from_display("H"), 119);
-    assert_eq!(from_display("I"), 0);
+    assert_eq!(from_display("I"), 95);
     // assert_eq!(from_display("J"), 0);
     // assert_eq!(from_display("K"), 0);
     // assert_eq!(from_display("L"), 0);
