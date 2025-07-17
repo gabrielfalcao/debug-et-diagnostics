f1d120c897c95e3cba30a9b30e6f77552a0391fa
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:00:42 +0000

    fix tests/test_color.rs line 36, such that "205);" becomes "80);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -33,7 +33,7 @@ use debug_et_diagnostics::{from_debug};
 #[test]
 fn test_from_debug_number_0_to_255() {
     assert_eq!(from_debug(0), 120);
-    assert_eq!(from_debug(1), 205);
+    assert_eq!(from_debug(1), 80);
     // assert_eq!(from_debug(2), 205);
     // assert_eq!(from_debug(3), 0);
     // assert_eq!(from_debug(4), 238);
