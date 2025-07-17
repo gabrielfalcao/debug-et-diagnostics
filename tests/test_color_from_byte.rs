eb803c6454fce7863a142653a0eef1ad82bbb74d
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:21:19 +0000

    fix tests/test_color_from_byte.rs line 6, such that "205);" becomes "1);"

--- tests/test_color_from_byte.rs
+++ tests/test_color_from_byte.rs
@@ -3,7 +3,7 @@ use debug_et_diagnostics::{from_byte, from_bytes};
 #[test]
 fn test_from_byte() {
     assert_eq!(from_byte(0), 0);
-    assert_eq!(from_byte(1), 205);
+    assert_eq!(from_byte(1), 1);
     // assert_eq!(from_byte(2), 205);
     // assert_eq!(from_byte(3), 0);
     // assert_eq!(from_byte(4), 238);
