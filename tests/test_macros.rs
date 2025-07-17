eecf6408a8092e2cdedd3368acd44fb857759b3f
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:54:16 +0000

    uncomment tests/test_macros.rs line 7 so as to trigger next fix

--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -4,7 +4,7 @@ use debug_et_diagnostics::tag;
 fn test_tag_open() {
     assert_eq!(tag!(@open, "text"), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m>\u{1b}[0m");
     assert_eq!(tag!(@open, "text", 220), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m>\u{1b}[0m");
-    // assert_eq!(tag!(@open, "text", @color=auto), "");
+    assert_eq!(tag!(@open, "text", @color=auto), "");
     // assert_eq!(tag!(@open, "text", @color=fore), "");
     // assert_eq!(tag!(@open, "text", @color=220), "");
     // assert_eq!(tag!(@open, "text", 220, @color=auto), "");
