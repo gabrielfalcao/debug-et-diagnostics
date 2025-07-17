c334cbd14b99f0c177b4080eb83f754eb907b888
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:20:56 +0000

    fix tests/test_color.rs line 450, such that "15);" becomes "80);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -447,7 +447,7 @@ fn test_from_debug_number_string_0_to_255() {
     assert_eq!(from_debug("152"), 120);
     assert_eq!(from_debug("153"), 80);
     assert_eq!(from_debug("154"), 15);
-    assert_eq!(from_debug("155"), 15);
+    assert_eq!(from_debug("155"), 80);
     assert_eq!(from_debug("156"), 215);
     assert_eq!(from_debug("157"), 255);
     assert_eq!(from_debug("158"), 135);
