7063974ac617046a5651d165e6c9c991542796a2
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:54:50 +0000

    fix tests/test_macros.rs line 27, such that """);" becomes ""\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;254m\<\\u{1b}[0m\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;254mtag\\u{1b}[0m\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;254m\>\\u{1b}[0m\\n    \\u{1b}[1;48;5;0m\\u{1b}[1;38;5;254mtext\\u{1b}[0m\\n\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;254m\<\/\\u{1b}[0m\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;254mtag\\u{1b}[0m\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;254m\>\\u{1b}[0m");"

--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -22,17 +22,17 @@ fn test_tag_close() {
     assert_eq!(tag!(@close, "text", 220, @color=fore), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m</\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m>\u{1b}[0m");
     assert_eq!(tag!(@close, "text", 220, @color=220), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m</\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m>\u{1b}[0m");
 }
-// #[test]
-// fn test_tag_wrap() {
-//     assert_eq!(tag!(@wrap, "tag", "text"), "");
-//     // assert_eq!(tag!(@wrap, "tag", "text", 220), "");
-//     // assert_eq!(tag!(@wrap, "tag", "text", @color=auto), "");
-//     // assert_eq!(tag!(@wrap, "tag", "text", @color=fore), "");
-//     // assert_eq!(tag!(@wrap, "tag", "text", @color=220), "");
-//     // assert_eq!(tag!(@wrap, "tag", "text", 220, @color=auto), "");
-//     // assert_eq!(tag!(@wrap, "tag", "text", 220, @color=fore), "");
-//     // assert_eq!(tag!(@wrap, "tag", "text", 220, @color=220), "");
-// }
+#[test]
+fn test_tag_wrap() {
+    assert_eq!(tag!(@wrap, "tag", "text"), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254mtag\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m>\u{1b}[0m\n    \u{1b}[1;48;5;0m\u{1b}[1;38;5;254mtext\u{1b}[0m\n\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m</\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254mtag\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m>\u{1b}[0m");
+    // assert_eq!(tag!(@wrap, "tag", "text", 220), "");
+    // assert_eq!(tag!(@wrap, "tag", "text", @color=auto), "");
+    // assert_eq!(tag!(@wrap, "tag", "text", @color=fore), "");
+    // assert_eq!(tag!(@wrap, "tag", "text", @color=220), "");
+    // assert_eq!(tag!(@wrap, "tag", "text", 220, @color=auto), "");
+    // assert_eq!(tag!(@wrap, "tag", "text", 220, @color=fore), "");
+    // assert_eq!(tag!(@wrap, "tag", "text", 220, @color=220), "");
+}
 
 // // #[test]
 // // fn test_tag_wrap() {
