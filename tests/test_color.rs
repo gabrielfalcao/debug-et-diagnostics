019798521e7c0c2fb92fa7b71d0014674db39219
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:12:55 +0000

    fix tests/test_color.rs line 279, such that "128);" becomes "40);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -276,7 +276,7 @@ fn test_from_debug_number_0_to_255() {
     assert_eq!(from_debug(241), 39);
     assert_eq!(from_debug(242), 95);
     assert_eq!(from_debug(243), 119);
-    assert_eq!(from_debug(244), 128);
+    assert_eq!(from_debug(244), 40);
     assert_eq!(from_debug(245), 138);
     assert_eq!(from_debug(246), 148);
     assert_eq!(from_debug(247), 158);
