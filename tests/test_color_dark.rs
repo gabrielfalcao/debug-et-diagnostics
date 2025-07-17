04d0c1b8904d084e08f0310a6f9fd617e327d8b8
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 20:01:09 +0000

    fix tests/test_color_dark.rs line 101, such that "96);" becomes "239);"

--- tests/test_color_dark.rs
+++ tests/test_color_dark.rs
@@ -98,7 +98,7 @@ fn test_dark() {
     assert_eq!(dark(93), 21);
     assert_eq!(dark(94), 22);
     assert_eq!(dark(95), 23);
-    assert_eq!(dark(96), 96);
+    assert_eq!(dark(96), 239);
     assert_eq!(dark(97), 97);
     assert_eq!(dark(98), 98);
     assert_eq!(dark(99), 99);
