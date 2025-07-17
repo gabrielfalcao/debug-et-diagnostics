63a4b18ba9403a00caf22795f5336f8342e621e2
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 20:00:26 +0000

    fix tests/test_color_bright.rs line 94, such that "89);" becomes "216);"

--- tests/test_color_bright.rs
+++ tests/test_color_bright.rs
@@ -91,7 +91,7 @@ fn test_bright() {
     assert_eq!(bright(86), 158);
     assert_eq!(bright(87), 159);
     assert_eq!(bright(88), 147);
-    assert_eq!(bright(89), 89);
+    assert_eq!(bright(89), 216);
     assert_eq!(bright(90), 90);
     assert_eq!(bright(91), 91);
     assert_eq!(bright(92), 92);
