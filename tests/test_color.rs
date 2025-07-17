419fd224b32b3f72ad9e1da9256ac064fc23b7e7
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:33:39 +0000

    fix tests/test_color.rs line 636, such that "15);" becomes "135);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -633,7 +633,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0x4b"), 215);
     assert_eq!(from_debug("0x4c"), 255);
     assert_eq!(from_debug("0x4d"), 216);
-    assert_eq!(from_debug("0x4e"), 15);
+    assert_eq!(from_debug("0x4e"), 135);
     assert_eq!(from_debug("0x4f"), 39);
     assert_eq!(from_debug("0x50"), 95);
     assert_eq!(from_debug("0x51"), 119);
