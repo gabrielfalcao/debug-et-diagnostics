10128f23d73ee43a7e8296de63ab2b4f9f4a2eb8
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:54:17 +0000

    uncomment tests/test_macros.rs line 8 so as to trigger next fix

--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -5,7 +5,7 @@ fn test_tag_open() {
     assert_eq!(tag!(@open, "text"), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m>\u{1b}[0m");
     assert_eq!(tag!(@open, "text", 220), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m>\u{1b}[0m");
     assert_eq!(tag!(@open, "text", @color=auto), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;195mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m>\u{1b}[0m");
-    // assert_eq!(tag!(@open, "text", @color=fore), "");
+    assert_eq!(tag!(@open, "text", @color=fore), "");
     // assert_eq!(tag!(@open, "text", @color=220), "");
     // assert_eq!(tag!(@open, "text", 220, @color=auto), "");
     // assert_eq!(tag!(@open, "text", 220, @color=fore), "");
