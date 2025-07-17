f79c324213c28c9c2634a349cee3c75f028a11e9
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:33:16 +0000

    fix tests/test_color.rs line 628, such that "240);" becomes "0);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -625,7 +625,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0x43"), 216);
     assert_eq!(from_debug("0x44"), 135);
     assert_eq!(from_debug("0x45"), 216);
-    assert_eq!(from_debug("0x46"), 240);
+    assert_eq!(from_debug("0x46"), 0);
     assert_eq!(from_debug("0x47"), 175);
     assert_eq!(from_debug("0x48"), 119);
     assert_eq!(from_debug("0x49"), 95);
