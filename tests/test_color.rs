891cbec4b9fbf4e77a5be3021af7b83d448e15e6
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:32:47 +0000

    fix tests/test_color.rs line 617, such that "95);" becomes "136);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -614,7 +614,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0x38"), 15);
     assert_eq!(from_debug("0x39"), 39);
     assert_eq!(from_debug("0x3a"), 240);
-    assert_eq!(from_debug("0x3b"), 95);
+    assert_eq!(from_debug("0x3b"), 136);
     assert_eq!(from_debug("0x3c"), 135);
     assert_eq!(from_debug("0x3d"), 175);
     assert_eq!(from_debug("0x3e"), 215);
