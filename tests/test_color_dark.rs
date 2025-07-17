10d1c03e13d5808fa2ba6d47bc10435b524ff004
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:59:32 +0000

    fix tests/test_color_dark.rs line 60, such that "55);" becomes "235);"

--- tests/test_color_dark.rs
+++ tests/test_color_dark.rs
@@ -57,7 +57,7 @@ fn test_dark() {
     assert_eq!(dark(52), 23);
     assert_eq!(dark(53), 16);
     assert_eq!(dark(54), 203);
-    assert_eq!(dark(55), 55);
+    assert_eq!(dark(55), 235);
     assert_eq!(dark(56), 56);
     assert_eq!(dark(57), 57);
     assert_eq!(dark(58), 58);
