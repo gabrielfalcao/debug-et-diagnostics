4e29425679f8635023d542b356cf324e79b6ac16
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:14:41 +0000

    fix tests/test_color.rs line 311, such that "0);" becomes "136);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -308,7 +308,7 @@ fn test_from_debug_number_string_0_to_255() {
     assert_eq!(from_debug("13"), 80);
     assert_eq!(from_debug("14"), 15);
     assert_eq!(from_debug("15"), 80);
-    assert_eq!(from_debug("16"), 0);
+    assert_eq!(from_debug("16"), 136);
     assert_eq!(from_debug("17"), 95);
     assert_eq!(from_debug("18"), 135);
     assert_eq!(from_debug("19"), 175);
