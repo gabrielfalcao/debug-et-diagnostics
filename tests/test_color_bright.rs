29253d56556b9e2e87fd69ab4482f0f00bfbf14b
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:58:36 +0000

    fix tests/test_color_bright.rs line 52, such that "47);" becomes "229);"

--- tests/test_color_bright.rs
+++ tests/test_color_bright.rs
@@ -49,7 +49,7 @@ fn test_bright() {
     assert_eq!(bright(44), 224);
     assert_eq!(bright(45), 225);
     assert_eq!(bright(46), 231);
-    assert_eq!(bright(47), 47);
+    assert_eq!(bright(47), 229);
     assert_eq!(bright(48), 48);
     assert_eq!(bright(49), 49);
     assert_eq!(bright(50), 50);
