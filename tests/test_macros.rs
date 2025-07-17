6241acf70c0bd68bdb003c4ca2c95c7336c83e8d
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:54:16 +0000

    fix tests/test_macros.rs line 6, such that """);" becomes ""\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;225m\<\\u{1b}[0m\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;225mtext\\u{1b}[0m\\u{1b}[1;48;5;0m\\u{1b}[1;38;5;225m\>\\u{1b}[0m");"

--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -3,7 +3,7 @@ use debug_et_diagnostics::tag;
 #[test]
 fn test_tag_open() {
     assert_eq!(tag!(@open, "text"), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m>\u{1b}[0m");
-    assert_eq!(tag!(@open, "text", 220), "");
+    assert_eq!(tag!(@open, "text", 220), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m<\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m>\u{1b}[0m");
     // assert_eq!(tag!(@open, "text", @color=auto), "");
     // assert_eq!(tag!(@open, "text", @color=fore), "");
     // assert_eq!(tag!(@open, "text", @color=220), "");
