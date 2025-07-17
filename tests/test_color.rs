27cb1cd8a854a285702091af26309b4b0da11a6f
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:49:10 +0000

    fix tests/test_color.rs line 792, such that "28);" becomes "175);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -789,7 +789,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0xe7"), 40);
     assert_eq!(from_debug("0xe8"), 80);
     assert_eq!(from_debug("0xe9"), 120);
-    assert_eq!(from_debug("0xea"), 28);
+    assert_eq!(from_debug("0xea"), 175);
     assert_eq!(from_debug("0xeb"), 38);
     assert_eq!(from_debug("0xec"), 48);
     assert_eq!(from_debug("0xed"), 58);
