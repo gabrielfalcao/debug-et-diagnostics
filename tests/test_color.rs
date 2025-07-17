e636eb34c85c3f1211b8eb3c31d766dacf306284
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:57:03 +0000

    uncomment tests/test_color.rs line 8 so as to trigger next fix

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -5,7 +5,7 @@ fn test_from_debug_letter_A_to_Z() {
     assert_eq!(from_debug("A"), 135);
     assert_eq!(from_debug("B"), 95);
     assert_eq!(from_debug("C"), 119);
-    // assert_eq!(from_debug("D"), 0);
+    assert_eq!(from_debug("D"), 0);
     // assert_eq!(from_debug("E"), 0);
     // assert_eq!(from_debug("F"), 255);
     // assert_eq!(from_debug("G"), 175);
