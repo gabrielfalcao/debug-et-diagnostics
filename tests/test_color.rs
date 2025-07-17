a56184684bfb9092d752e037076c8c55f0b0a075
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:33:22 +0000

    fix tests/test_color.rs line 630, such that "119);" becomes "80);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -627,7 +627,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0x45"), 216);
     assert_eq!(from_debug("0x46"), 0);
     assert_eq!(from_debug("0x47"), 40);
-    assert_eq!(from_debug("0x48"), 119);
+    assert_eq!(from_debug("0x48"), 80);
     assert_eq!(from_debug("0x49"), 95);
     assert_eq!(from_debug("0x4a"), 39);
     assert_eq!(from_debug("0x4b"), 15);
