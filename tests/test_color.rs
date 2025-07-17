f15ff07830225f73d9b88008fd8e00a1d63d4721
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:50:01 +0000

    fix tests/test_color.rs line 810, such that "208);" becomes "39);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -807,7 +807,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0xf9"), 160);
     assert_eq!(from_debug("0xfa"), 119);
     assert_eq!(from_debug("0xfb"), 15);
-    assert_eq!(from_debug("0xfc"), 208);
+    assert_eq!(from_debug("0xfc"), 39);
     assert_eq!(from_debug("0xfd"), 218);
     assert_eq!(from_debug("0xfe"), 228);
     assert_eq!(from_debug("0xff"), 238);
