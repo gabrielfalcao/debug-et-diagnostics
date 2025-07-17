5cb9080f0236ed5cb3f174400c3de285696a74d0
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:45:27 +0000

    fix tests/test_color.rs line 695, such that "119);" becomes "175);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -692,7 +692,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0x86"), 215);
     assert_eq!(from_debug("0x87"), 255);
     assert_eq!(from_debug("0x88"), 135);
-    assert_eq!(from_debug("0x89"), 119);
+    assert_eq!(from_debug("0x89"), 175);
     assert_eq!(from_debug("0x8a"), 175);
     assert_eq!(from_debug("0x8b"), 135);
     assert_eq!(from_debug("0x8c"), 255);
