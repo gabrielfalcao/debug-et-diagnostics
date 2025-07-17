f40fb1158a55c2227743fa44ee6295df1167bdff
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:54:38 +0000

    uncomment tests/test_macros.rs line 23 so as to trigger next fix

--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -20,7 +20,7 @@ fn test_tag_close() {
     assert_eq!(tag!(@close, "text", @color=220), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m</\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;254m>\u{1b}[0m");
     assert_eq!(tag!(@close, "text", 220, @color=auto), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m</\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;195mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m>\u{1b}[0m");
     assert_eq!(tag!(@close, "text", 220, @color=fore), "\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m</\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225mtext\u{1b}[0m\u{1b}[1;48;5;0m\u{1b}[1;38;5;225m>\u{1b}[0m");
-    // assert_eq!(tag!(@close, "text", 220, @color=220), "");
+    assert_eq!(tag!(@close, "text", 220, @color=220), "");
 }
 // #[test]
 // fn test_tag_wrap() {
