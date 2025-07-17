10e50b079f334bf2d05126daab543ea25271460c
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:57:10 +0000

    fix tests/test_color_bright.rs line 20, such that "15);" becomes "255);"

--- tests/test_color_bright.rs
+++ tests/test_color_bright.rs
@@ -17,7 +17,7 @@ fn test_bright() {
     assert_eq!(bright(12), 147);
     assert_eq!(bright(13), 231);
     assert_eq!(bright(14), 255);
-    assert_eq!(bright(15), 15);
+    assert_eq!(bright(15), 255);
     assert_eq!(bright(16), 16);
     assert_eq!(bright(17), 17);
     assert_eq!(bright(18), 18);
