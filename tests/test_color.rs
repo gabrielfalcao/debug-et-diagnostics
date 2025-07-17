60aec2b9489e09c303b1acdb9fc276967ac020a7
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:49:30 +0000

    fix tests/test_color.rs line 799, such that "98);" becomes "80);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -796,7 +796,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0xee"), 135);
     assert_eq!(from_debug("0xef"), 95);
     assert_eq!(from_debug("0xf0"), 120);
-    assert_eq!(from_debug("0xf1"), 98);
+    assert_eq!(from_debug("0xf1"), 80);
     assert_eq!(from_debug("0xf2"), 108);
     assert_eq!(from_debug("0xf3"), 118);
     assert_eq!(from_debug("0xf4"), 128);
