806703f5225df7b392d85944ffcad884a25d1546
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:48:04 +0000

    fix tests/test_color.rs line 766, such that "120);" becomes "255);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -763,7 +763,7 @@ fn test_from_debug_number_string_0x00_to_0xff() {
     assert_eq!(from_debug("0xcd"), 160);
     assert_eq!(from_debug("0xce"), 255);
     assert_eq!(from_debug("0xcf"), 39);
-    assert_eq!(from_debug("0xd0"), 120);
+    assert_eq!(from_debug("0xd0"), 255);
     assert_eq!(from_debug("0xd1"), 39);
     assert_eq!(from_debug("0xd2"), 255);
     assert_eq!(from_debug("0xd3"), 215);
