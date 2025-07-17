61d215da7dc7d52097208529c76a599692d201ae
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 19:10:21 +0000

    fix tests/test_color.rs line 220, such that "95);" becomes "216);"

--- tests/test_color.rs
+++ tests/test_color.rs
@@ -217,7 +217,7 @@ fn test_from_debug_number_0_to_255() {
     assert_eq!(from_debug(182), 240);
     assert_eq!(from_debug(183), 216);
     assert_eq!(from_debug(184), 135);
-    assert_eq!(from_debug(185), 95);
+    assert_eq!(from_debug(185), 216);
     assert_eq!(from_debug(186), 135);
     assert_eq!(from_debug(187), 175);
     assert_eq!(from_debug(188), 215);
