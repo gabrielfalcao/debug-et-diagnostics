b113ebc159f85df5c08a57f854f2d9b1243f4fc2
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 18:26:01 +0000

    uncomment tests/test_color_from_byte.rs line 225 so as to trigger next fix

--- tests/test_color_from_byte.rs
+++ tests/test_color_from_byte.rs
@@ -222,7 +222,7 @@ fn test_from_byte() {
     assert_eq!(from_byte(217), 217);
     assert_eq!(from_byte(218), 218);
     assert_eq!(from_byte(219), 219);
-    // assert_eq!(from_byte(220), 40);
+    assert_eq!(from_byte(220), 40);
     // assert_eq!(from_byte(221), 119);
     // assert_eq!(from_byte(222), 175);
     // assert_eq!(from_byte(223), 135);
