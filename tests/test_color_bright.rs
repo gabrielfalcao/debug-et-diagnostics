176ba9f30aeac71568252f73590f6da42bc5d0d0
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 20:02:29 +0000

    fix tests/test_color_bright.rs line 173, such that "168);" becomes "180);"

--- tests/test_color_bright.rs
+++ tests/test_color_bright.rs
@@ -170,7 +170,7 @@ fn test_bright() {
     assert_eq!(bright(165), 195);
     assert_eq!(bright(166), 183);
     assert_eq!(bright(167), 181);
-    assert_eq!(bright(168), 168);
+    assert_eq!(bright(168), 180);
     assert_eq!(bright(169), 169);
     assert_eq!(bright(170), 170);
     assert_eq!(bright(171), 171);
