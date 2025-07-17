907480328348b1bd75350c619a799e45f60152ac
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-17 17:36:25 +0000

    fix tests/test_color_get_ansi_rgb.rs line 211, such that "[0, 95, 0]);" becomes "[255, 95, 0]);"

--- tests/test_color_get_ansi_rgb.rs
+++ tests/test_color_get_ansi_rgb.rs
@@ -208,7 +208,7 @@ fn test_cube() {
     assert_get_ansi_rgb!(199, [255, 0, 175]);
     assert_get_ansi_rgb!(200, [255, 0, 215]);
     assert_get_ansi_rgb!(201, [255, 0, 255]);
-    assert_get_ansi_rgb!(202, [0, 95, 0]);
+    assert_get_ansi_rgb!(202, [255, 95, 0]);
     assert_get_ansi_rgb!(203, [0, 95, 95]);
     assert_get_ansi_rgb!(204, [0, 95, 135]);
     assert_get_ansi_rgb!(205, [0, 95, 175]);
