7022f5051bf30cdcbb34193f62ab6f5e4a9f9d49
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-23 04:34:35 +0000

    uncomment tests/test_color_get_ansi_rgb.rs line 28 so as to trigger next fix

--- tests/test_color_get_ansi_rgb.rs
+++ tests/test_color_get_ansi_rgb.rs
@@ -25,7 +25,7 @@ use debug_et_diagnostics::get_ansi_rgb;
 fn test_cube() {
     assert_get_ansi_rgb!(17, [0, 0, 95]);
     assert_get_ansi_rgb!(18, [0, 0, 135]);
-    // assert_get_ansi_rgb!(19, [215, 135, 175]);
+    assert_get_ansi_rgb!(19, [215, 135, 175]);
     // assert_get_ansi_rgb!(20, [255, 175, 95]);
     // assert_get_ansi_rgb!(21, [0, 0, 255]);
     // assert_get_ansi_rgb!(22, [0, 95, 0]);
