72a89bfbbf7668684ee17338e769b4bd2cc05a8e
Gabriel Falcão <gabrielteratos@gmail.com>
2025-07-23 03:09:53 +0000

    fix tests/test_color_dark.rs line 12, such that "237);" becomes "16);"

--- tests/test_color_dark.rs
+++ tests/test_color_dark.rs
@@ -9,7 +9,7 @@ fn test_dark() {
     assert_eq!(dark(4), 16);
     assert_eq!(dark(5), 16);
     assert_eq!(dark(6), 16);
-    assert_eq!(dark(7), 237);
+    assert_eq!(dark(7), 16);
     // assert_eq!(dark(8), 232);
     // assert_eq!(dark(9), 196);
     // assert_eq!(dark(10), 46);
